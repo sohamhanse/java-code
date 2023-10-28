7a9af9150c81f0ca2b617bd3a19ae278bdd3207a
sohamhanse <sohamh_c22087@students.isquareit.edu.in>
2023-10-28 23:02:47 +0530

    Create fraction_knapsnap.java

---
+++ greedy_algo/fraction_knapsnap.java
@@ -0,0 +1,37 @@
+package greedy_algo;
+
+import java.util.*;
+
+public class fraction_knapsnap {
+    public static void main(String arg[]){
+        int weight[] = {20,10,30};
+        int value [] = {100 ,60 ,120};
+        int activity [][] = new int[weight.length][3] ;
+        int bagweigth = 50;
+
+        for (int i = 0; i < weight.length; i++) {
+            activity[i][0] = weight[i];
+            activity[i][1] = value[i];
+            activity[i][2] = activity[i][1] / activity[i][0];
+        }
+
+        Arrays.sort(activity , Comparator.comparingDouble(o -> o[2]));
+
+        int val = 0 ;
+        int capacity = 0;
+        for(int i = weight.length-1 ; i>=0 ;i--){
+            if(capacity == bagweigth){
+                break;
+            }
+            int remain = bagweigth-capacity;
+            if(activity[i][0] <= remain){
+                val += activity[i][1];
+                capacity += activity[i][0];
+            }else{
+                val += remain * activity[i][2];
+                capacity += remain;
+            }
+        }
+        System.out.println(val);
+    }
+}
