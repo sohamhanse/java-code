a6e7ab50170a8cba5d87856451beddea48a24ee7
sohamhanse <sohamh_c22087@students.isquareit.edu.in>
2023-12-04 00:55:16 +0530

    hashmap

---
+++ hashmap/LRUCache.java
@@ -0,0 +1,9 @@
+package hashmap;
+
+import java.util.HashMap;
+
+public class LRUCache {
+    public static void main(String arg[]){
+       // https://leetcode.com/problems/lru-cache/submissions/1111424957
+    }
+}
